c25d4e7e46c35c571a08e04982214f844f8f42ec
wook <edisen315@gmail.com>
2024-04-27 20:09:29 +0900

    JS, popup<->Content 통신완료

--- popup.js
+++ popup.js
@@ -1,2 +1,8 @@
-console.log("This is a popup!");
-alert('this is a popup');
+    
+    console.log("popup.js start");
+    // popup.js
+    document.getElementById('toggleButton').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "toggleElement"});
+        });
+    });
--- scripts/content.js
+++ scripts/content.js
@@ -1,27 +1,48 @@
 window.onload = function() {
-  console.log('======');
-  const curriculm = document.querySelector('[title="커리큘럼"]'); // 특정 속성을 기반으로 선택
-  curriculm.click();
+  // 랜덤 색상 생성 함수
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-  setTimeout(function() {
-    const small_lecture = document.querySelector('li[aria-current="true"][data-testid="sectionUnit"]').textContent;
-    console.log("소강의 : " + small_lecture);
+  console.log('====== content.js start ======');
 
-    const big_lecture = document.querySelector('.css-1pqj6dl').innerText;
-    console.log("대강의 : " + big_lecture);
-    console.log('======');
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action == "toggleElement") {
+      // 요소 토글 로직
+      console.log("수신완료!");
+      document.querySelector('.mantine-Tabs-tabsList.mantine-knbi38').style.backgroundColor = getRandomColor();
+    }
+  }
+);
 
-    // background.js 와 연결
-    console.log("send Message to Background Worker From content");
-    chrome.runtime.sendMessage({greeting: "hello", small_lecture : small_lecture}, function(response){
+  // setTimeout(function() {
+  //   const curriculm = document.querySelector('[title="커리큘럼"]'); // 특정 속성을 기반으로 선택
+  //   curriculm.click();
+  //   const small_lecture = document.querySelector('li[aria-current="true"][data-testid="sectionUnit"]').textContent;
+  //   console.log("소강의 : " + small_lecture);
+
+  //   const big_lecture = document.querySelector('.css-1pqj6dl').innerText;
+  //   console.log("대강의 : " + big_lecture);
+  //   console.log('======');
+
+  //   // background.js 와 연결
+  //   console.log("send Message to Background Worker From content");
+  //   chrome.runtime.sendMessage({greeting: "hello", small_lecture : small_lecture}, function(response){
       
-    console.log(response.farewell);
+  //   console.log(response.farewell);
 
 
       
-    });
+  //   });
 
-  }, 3000);
+  // }, 3000);
 
 
 
