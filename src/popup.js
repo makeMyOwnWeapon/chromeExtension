96070b8815a5ea811c30e19d50e7e53322c71b3f
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-30 01:37:29 +0900

    fix: popup

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,25 @@
+document.getElementById('linkAccount').addEventListener('click', function() {
+    var authCode = document.getElementById('authCode').value;
+    var url = `http://localhost:3000/api/auth/extension`;
+    fetch(url, {
+        headers: {
+            'Authorization': authCode
+        }
+    })
+    .then(response => {
+        if (response.status === 200) {
+            document.getElementById('controlButtons').style.display = 'block';
+        } else if (response.status === 204) {
+            chrome.tabs.update({url: 'http://localhost:3000/main'});
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
 document.getElementById('activate').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {command: 'turnOn'}, function(response) {
-            if (response.result === "already active") {
+            if (response && response.result === "already active") {
                 alert("Already activated!");
             }
         });
