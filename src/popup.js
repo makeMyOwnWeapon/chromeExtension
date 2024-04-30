ae099aedc4296806d7bb0f0846d759125c1c0f08
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-30 23:58:05 +0900

    fix: popupdetail

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function updateLinkButton(isConnected) {
+        var linkButton = document.getElementById('linkAccount');
+        linkButton.textContent = isConnected ? "연결 끊기" : "계정 연결";
+    }
+
+    function toggleNewAccountButton(display) {
+        document.getElementById('imNew').style.display = display ? 'block' : 'none';
+    }
+
     chrome.storage.local.get('authToken', function(data) {
         if (data.authToken) {
             document.getElementById('controlButtons').style.display = 'block';
+            updateLinkButton(true);
+            toggleNewAccountButton(false);
         }
     });
 
@@ -9,32 +20,41 @@ document.addEventListener('DOMContentLoaded', function() {
         var authCode = document.getElementById('authCode').value;
         var url = `http://localhost:3000/api/auth/extension`;
 
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': authCode
-            }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            chrome.storage.local.set({'authToken': data.token}, () => {
-                document.getElementById('controlButtons').style.display = 'block';
-                document.getElementById('authMessage').style.display = 'none';
+        if (this.textContent === "계정 연결") {
+            fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': authCode
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                chrome.storage.local.set({'authToken': data.token}, () => {
+                    document.getElementById('controlButtons').style.display = 'block';
+                    document.getElementById('authMessage').style.display = 'none';
+                    updateLinkButton(true);
+                    toggleNewAccountButton(false);
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById('authMessage').style.display = 'block';
+                document.getElementById('authMessage').textContent = '인증 실패! 다시 시도해주세요.';
             });
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('authMessage').style.display = 'block';
-            document.getElementById('authMessage').textContent = 'Network or server error.';
-        });
+        } else {
+            chrome.storage.local.remove('authToken', () => {
+                document.getElementById('controlButtons').style.display = 'none';
+                updateLinkButton(false);
+                toggleNewAccountButton(true);
+            });
+        }
     });
-    
     document.getElementById('showToken').addEventListener('click', function() {  //임시 코드
         chrome.storage.local.get('authToken', function(data) {
             if (data.authToken) {
