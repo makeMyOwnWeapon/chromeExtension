09df11373c0503d4b436436a7f0af2a8cdb1cf1a
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-06 01:15:04 +0900

    git push alarm

--- scripts/contentScripts/content.js
+++ scripts/contentScripts/content.js
@@ -1,11 +1,13 @@
 import '../../scss/main.scss';
 import { addLearningAssistantIcon, removeLearningAssistantIcon } from './icon/icon.js';
-import {turnOnLogging, turnOffLogging} from './subtitle/logging.js';
+// import {turnOnLogging, turnOffLogging} from './subtitle/logging.js';
+import { showWakeUpModal } from './alarm/wakeupmodal.js';
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.command === 'turnOn') {
         addLearningAssistantIcon();
-        // turnOnLogging();
+        showWakeUpModal();
+        turnOnLogging();
         sendResponse({result: "success"});
 
     } else if (request.command === 'turnOff') {
