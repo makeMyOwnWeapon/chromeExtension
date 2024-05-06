17167a84791a42ab660e221660583929a8d347d5
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-06 14:34:59 +0900

    fix: serviceworker

---
+++ scripts/serviceWorkers/serviceWorker.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type === 'REST') {
+        fetch(request.url, request.options)
+            .then(resp => resp.json())
+            .then(data => {
+                // json 데이터 추출 
+                sendResponse(data);
+            });
+    }
+    return true;
+});
