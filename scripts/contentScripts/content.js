7f85658922152c427141e7423493be86c4ee83f2
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-08 00:48:56 +0900

    chore: 학습 시작일때 소켓연결

--- scripts/contentScripts/content.js
+++ scripts/contentScripts/content.js
@@ -1,6 +1,5 @@
 import '../../scss/main.scss';
 import { addLearningAssistantIcon, removeLearningAssistantIcon } from './icon/icon.js';
-// import { connect, disconnect } from './connection/connection.js'
 // import {turnOnLogging, turnOffLogging} from './subtitle/logging.js';
 // import { showWakeUpModal } from './alarm/wakeupmodal.js';
 
@@ -8,14 +7,11 @@ import { addLearningAssistantIcon, removeLearningAssistantIcon } from './icon/ic
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.command === 'turnOn') {
         addLearningAssistantIcon();
-        // connect();
-        // showWakeUpModal();
         // turnOnLogging();
         sendResponse({result: "success"});
 
     } else if (request.command === 'turnOff') {
         removeLearningAssistantIcon();
-        // disconnect();
         // turnOffLogging();
         sendResponse({result: "success"});
     }
