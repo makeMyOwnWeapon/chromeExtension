5a0d59090cea0ae266e4534fbbd3240f4154a0c5
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-08 13:24:15 +0900

    del: historyid

--- scripts/contentScripts/connection/connection.js
+++ scripts/contentScripts/connection/connection.js
@@ -14,13 +14,10 @@ function connect() {
         alert('소켓 연결이 되었습니다!');
         chrome.storage.local.get('authToken', function(data) {
             const authToken = data.authToken;
-            // lectureHistoryId를 임시 값으로 설정, 예를 들어 12345
-            const lectureHistoryId = 12345;  // 이 값을 진짜 lecture history id로 교체하세요
             socket.emit('sendData', { 
                 socketId: socket.id, 
                 token: authToken, 
                 subLectureId: workbookContext.subLectureId || "default",
-                lectureHistoryId: lectureHistoryId  // 임시값 추가
             });
         });
     });
