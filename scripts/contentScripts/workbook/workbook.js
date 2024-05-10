4ac4c76bca107872d11a1d51a842e52b16e84873
koesnam <76582376+10kseok@users.noreply.github.com>
2024-05-10 21:41:02 +0900

    test: 서버에 1초마다 영상전송

--- scripts/contentScripts/workbook/workbook.js
+++ scripts/contentScripts/workbook/workbook.js
@@ -89,12 +89,44 @@ function makeWorkbookHTML_TOBE() {
         <button class="btn analysis-btn" type="button" id="analysis-end-btn">
             <span> 학습 종료 </span>
         </button>
+        <video autoplay style="width: 100%;" class="web-cam"></video>
     </div>
     `;
+}
+
+function captureAndSendImages(video) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    console.log('video', video.width, video.height);
+    console.log('canvase', canvas.width, canvas.height);
+    const capture = () => {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        LoaAxios.postFile(
+            `${HOST}/api/video`,
+            canvas.toDataURL('image/jpeg'), 
+            (response) => console.log(response.result)
+        );
+    };
+    // setTimeout(capture, 2000);
+    setInterval(capture, 1000);
 }
 
 export function displayWorkbookContent() {
     updateWorkbookContent(makeWorkbookHTML_TOBE());
     addQuizsetsAndRender(URLParser.parseWithoutTab(document.location.href));
     refreshAnalysisBtn();
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function(stream) {
+            // 스트림 사용, 예를 들어 비디오 태그에 연결
+            let video = document.querySelector('.web-cam');
+            video.srcObject = stream;
+            video.onloadedmetadata = function(e) {
+                video.play();
+            };
+            captureAndSendImages(video);
+        })
+        .catch(function(err) {
+            console.log("getUserMedia Error: " + err);
+        });   
 }
+
--- scripts/serviceWorkers/serviceWorker.js
+++ scripts/serviceWorkers/serviceWorker.js
@@ -10,5 +10,36 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 sendResponse(error);
             });
     }
+
+    if (request.type === "REST_FILE") {
+        const base64Data = request.base64Data;
+        const parts = base64Data.split(';base64,');
+        const imageData = parts[1];
+        const blob = base64ToBlob(imageData, 'image/jpeg');
+        const formData = new FormData();
+        formData.append('file', blob, 'temp.jpeg');
+        fetch(request.url, {method: 'POST', body: formData})
+            .then(resp => resp.json())
+            .then(data => {
+                // json 데이터 추출 
+                sendResponse(data);
+            })
+            .catch(error => {
+                sendResponse(error);
+            });
+    } 
     return true;
 });
+
+
+// Base64 문자열을 Blob으로 변환하는 함수
+function base64ToBlob(base64, mimeType) {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    
+    return new Blob([byteArray], {type: mimeType});
+  }
