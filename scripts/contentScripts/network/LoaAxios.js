fccf0d5cd67e28c67488d14c68eafc2579d99ec3
wook <edisen315@gmail.com>
2024-05-10 22:17:40 +0900

    t2.xlarge로 변경

--- scripts/contentScripts/network/LoaAxios.js
+++ scripts/contentScripts/network/LoaAxios.js
@@ -86,5 +86,5 @@ export const LoaAxios = (() => {
 
 export const HOST = {
     local: 'http://localhost:3000',
-    prod: 'http://13.124.188.208:3000'
+    prod: 'http://52.79.124.34:3000'
 }.prod;
