7afa62b099dc6fe9f11bc47ba7d805eccdfa4367
jeemh <jimyeongheon@gmail.com>
2024-05-22 22:37:59 +0900

    feat: 로드할 때 스크립트 버튼 두 번 클릭

--- scripts/contentScripts/content.js
+++ scripts/contentScripts/content.js
@@ -4,6 +4,29 @@ import { addLearningAssistantIcon, removeLearningAssistantIcon } from './icon/ic
 
 window.onload = function() {
     setTimeout(function() {
+
+        // 모든 'css-zl1inp' 클래스를 가진 'li' 요소를 찾습니다.
+        const listItems = document.querySelectorAll('li.css-zl1inp');
+
+        // 다섯 번째 'li' 요소가 존재하는지 확인합니다.
+        if (listItems.length >= 5) {
+            const fifthListItem = listItems[4];
+
+            // 다섯 번째 'li' 요소 내부에서 버튼을 찾습니다.
+            const button = fifthListItem.querySelector('.mantine-UnstyledButton-root.mantine-Button-root.mantine-syxma7');
+
+            // 버튼이 존재하면 클릭 이벤트를 트리거합니다.
+            if (button) {
+                button.click();
+                setTimeout(function() {button.click()}, 0);
+                
+            } else {
+                console.error('해당 요소에 버튼이 없습니다');
+            }
+        } else {
+            console.error('5개의 버튼이 없습니다');
+        }
+
         chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             if (request.command === 'turnOn') {
                 chrome.storage.local.set({ iconDisabled: false }, function() {
